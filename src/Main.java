8a501a3e43b8b79ee5bf1f3b72c5da93294bf0ce
ArtemPankratov <32225670+ArtemPankratov@users.noreply.github.com>
2017-10-10 19:56:04 -0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,107 @@
+import java.util.Scanner;
+
+/**
+ * Exercises on if, for and while loops
+ */
+public class Main {
+    public static void main(String[] args) {
+        //TODO: Write a Java program to compare two numbers.
+        Scanner in = new Scanner(System.in);
+        /*System.out.println("Enter two numbers: ");
+        int number1 = in.nextInt();
+        int number2 = in.nextInt();
+        if(number1 > number2)
+
+        {
+        System.out.println(number1 + " is greater than " + number2);
+        }
+        else if(number1 < number2)
+
+        {
+        System.out.println(number2 + " is greater than " + number1);
+        }
+        else
+
+        {
+        System.out.println(number1 + " is equal to " + number2);
+        }
+        }
+        */
+        // TODO: Write a Java program that reads a floating-point number and prints "zero" if the number is zero.
+        // TODO: Otherwise, print "positive" or "negative".
+        /* System.out.println("Enter the number: ");
+
+
+        double num = in.nextDouble();
+
+
+        if (num == 0) {
+
+
+        System.out.println("zero");
+
+
+        } else if (num > 0) {
+
+
+        System.out.println("positive");
+
+
+        } else {
+
+
+        System.out.println("negative");
+
+
+        }
+
+
+        }
+        */
+
+
+        // TODO: Write a program in Java to display the first 10 natural numbers.
+        /* int num;
+
+
+        System.out.println("The first 10 natural numbers: ");
+
+
+        for (num = 1; num <= 9; num++) {
+
+        System.out.println(num);
+
+
+        }
+
+
+        System.out.println(num);
+
+
+        }
+
+
+        */
+
+
+        // TODO: Write a Java program to reverse a string. Print the before and after.
+        /*System.out.println("Enter a string: ");
+        String string = in.nextLine();
+        StringBuilder reversedstring = new StringBuilder();
+
+        // convert a string into StringBuilder string in order to use reverse() command
+        reversedstring.append(string);
+
+        // reverse StringBuilder string
+        reversedstring = reversedstring.reverse();
+
+
+        for (int i=0; i<reversedstring.length(); i++)
+            //get length
+            System.out.print(reversedstring.charAt(i));
+        //charAt returns a character at the specified index
+        //program prints out reversed string
+        */
+
+
+    }}
